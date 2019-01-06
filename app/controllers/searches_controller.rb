5ca4e2a8bc868a1021a2bb7df2f631aecfaf479e
ryugomikami <mikamiryuugo@yahoo.co.jp>
2019-01-06 02:51:18 +0000

    セレクトタグの中に空白行、「全員」を追加して希望通りの検索ができてる気がする

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,12 +1,11 @@
 class SearchesController < ApplicationController
     
  def index
-    search_name = params[:name]
+    search_name = params[:name] unless params[:name] == " " || params[:name] == "全員"
     search_date = params[:date]
     if search_name && search_date
       search_user = User.joins(:profile).find_by(profiles: {name: search_name})
       @memos = search_user.memos.where(recorded_on: search_date.to_date)
-      # @memos = search_user.memos
     elsif search_name
       search_user = User.joins(:profile).find_by(profiles: {name: search_name})
       @memos = search_user.memos
@@ -15,11 +14,8 @@ class SearchesController < ApplicationController
     else
       @memos = current_user.feed_memos.order('created_at DESC')
     end
-    
+    # @users_id = User.all.map {|user| user.id}
     @users_name = User.all.map { |user| user.profile.name }
-    
-    #上の@users_nameの中に、ハッシュの配列を入れるのかな？そうすれば表示名と値をわけられる。
-    #そしたら検索部分もuser_id: になるのかな
-    # @users_id =  User.all.map {|user| user.id}
+    @users_name = @users_name.unshift(" ", "全員")
  end
 end
